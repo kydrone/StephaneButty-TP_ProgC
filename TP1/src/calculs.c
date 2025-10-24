c3f53d2584b7372d8f31738e4fec69062c564fdc
Stephane butty <41823736+kydrone@users.noreply.github.com>
2025-10-24 11:53:39 +0200

    Implement basic calculator with multiple operations / Calculs.c

--- TP1/src/calculs.c
+++ TP1/src/calculs.c
@@ -1,1 +1,53 @@
+#include <stdio.h>
 
+int main() {
+    int num1 = 12;
+    int num2 = 5;
+    char op = '+'; 
+    int resultat = 0;
+    switch (op) {
+        case '+':
+            resultat = num1 + num2;
+            printf("%d + %d = %d\n", num1, num2, resultat);
+            break;
+        case '-':
+            resultat = num1 - num2;
+            printf("%d - %d = %d\n", num1, num2, resultat);
+            break;
+        case '*':
+            resultat = num1 * num2;
+            printf("%d * %d = %d\n", num1, num2, resultat);
+            break;
+        case '/':
+            if (num2 != 0) {
+                resultat = num1 / num2;
+                printf("%d / %d = %d\n", num1, num2, resultat);
+            } else {
+                printf("Erreur : division par zéro\n");
+            }
+            break;
+        case '%':
+            if (num2 != 0) {
+                resultat = num1 % num2;
+                printf("%d %% %d = %d\n", num1, num2, resultat);
+            } else {
+                printf("Erreur : modulo par zéro\n");
+            }
+            break;
+        case '&':
+            resultat = num1 & num2;
+            printf("%d & %d = %d\n", num1, num2, resultat);
+            break;
+        case '|':
+            resultat = num1 | num2;
+            printf("%d | %d = %d\n", num1, num2, resultat);
+            break;
+        case '~':
+            resultat = ~num1;
+            printf("~%d = %d\n", num1, resultat);
+            break;
+        default:
+            printf("Opérateur non reconnu : %c\n", op);
+    }
+    return 0;
+}
