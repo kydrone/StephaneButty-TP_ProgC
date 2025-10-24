26db37599822bf70198079505691529cb8d910b5
Stephane butty <41823736+kydrone@users.noreply.github.com>
2025-10-24 10:34:28 +0200

    Correction Edit Opérateur.c
    Added boolean checks for equality and superiority between a and b.

--- TP1/src/opérateurs.c
+++ TP1/src/opérateurs.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
-#include <stdbool.h>
-
+#include <stdbool.h> // Pour utiliser le type booléen
 int main() {
     int a = 16;
     int b = 3;
@@ -9,13 +8,15 @@ int main() {
     int multiplication = a * b;
     int division = a / b;
     int modulo = a % b;
-    printf("Valeurs initiales : ", a, b);
-    printf("Addition (a + b) : ", addition);
-    printf("Soustraction (a - b) :", soustraction);
-    printf("Multiplication (a * b) : ", multiplication);
-    printf("Division (a / b) : ", division);
-    printf("Modulo (a %% b) : ", modulo);
-    printf("a est-il égal à b ? : ", egal ? "true" : "false");
-    printf("a est-il supérieur à b ? : ", superieur ? "true" : "false");
+    bool egal = (a == b);  
+    bool superieur = (a > b); 
+    printf("Valeurs initiales : a = %d, b = %d\n\n", a, b);
+    printf("Addition (a + b)         : %d\n", addition);
+    printf("Soustraction (a - b)     : %d\n", soustraction);
+    printf("Multiplication (a * b)   : %d\n", multiplication);
+    printf("Division (a / b)         : %d\n", division);
+    printf("Modulo (a %% b)           : %d\n\n", modulo); // %% pour afficher %
+    printf("a est-il égal à b ?      : %s\n", egal ? "true" : "false");
+    printf("a est-il supérieur à b ? : %s\n", superieur ? "true" : "false");
     return 0;
 }
