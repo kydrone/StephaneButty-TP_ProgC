e78d7fc5f3919dd03cad980079c114f54a628242
Stephane butty <41823736+kydrone@users.noreply.github.com>
2025-10-24 10:31:43 +0200

    Edit Opérateurs.c
    Implement basic arithmetic operations and comparisons

---
+++ TP1/src/opérateurs.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+    int a = 16;
+    int b = 3;
+    int addition = a + b;
+    int soustraction = a - b;
+    int multiplication = a * b;
+    int division = a / b;
+    int modulo = a % b;
+    printf("Valeurs initiales : ", a, b);
+    printf("Addition (a + b) : ", addition);
+    printf("Soustraction (a - b) :", soustraction);
+    printf("Multiplication (a * b) : ", multiplication);
+    printf("Division (a / b) : ", division);
+    printf("Modulo (a %% b) : ", modulo);
+    printf("a est-il égal à b ? : ", egal ? "true" : "false");
+    printf("a est-il supérieur à b ? : ", superieur ? "true" : "false");
+    return 0;
+}
